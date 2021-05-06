ab7bdb2d0e7a418979704dae97e47578339f1712
samj0516 <samj0516@gmail.com>
2021-05-06 15:07:59 -0500

    completed ch7 list of games rendered on client

--- levelup/urls.py
+++ levelup/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user
 from rest_framework import routers
-from levelupapi.views import GameTypes
+from levelupapi.views import GameTypes, GameView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypes, 'gametype')
-
+router.register(r'games', GameView, 'game')
 urlpatterns = [
     path('', include(router.urls)),
     path('register', register_user),
