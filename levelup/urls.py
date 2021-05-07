5d4a0477235a831c310afe4c539af43b2b3d34f2
samj0516 <samj0516@gmail.com>
2021-05-07 12:33:46 -0500

    finished ch8 events render on client side

--- levelup/urls.py
+++ levelup/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user
 from rest_framework import routers
-from levelupapi.views import GameTypes, GameView
+from levelupapi.views import GameTypes, GameView, EventView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypes, 'gametype')
 router.register(r'games', GameView, 'game')
+router.register(r'events', EventView, 'event')
 urlpatterns = [
     path('', include(router.urls)),
     path('register', register_user),
